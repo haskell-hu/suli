9029ad0d2fba15f3b9b73e40e0e37e7a147a139d
Robin Palotai <palotai.robin@gmail.com>
2016-07-13 20:32:56 +0200

    Komment az időalapú szótagoláshoz.

--- src/Suli/Nyelv/Verslab.hs
+++ src/Suli/Nyelv/Verslab.hs
@@ -1,3 +1,7 @@
+{-
+Modul verses szövegek verslábakra bontásához.
+Lásd https://hu.wikipedia.org/wiki/Időmértékes_verselés.
+-}
 {-# LANGUAGE OverloadedStrings #-}
 module Suli.Nyelv.Verslab where
 
@@ -25,21 +29,40 @@ hosszuMaganhangzok =
 maganhangzok :: [Char]
 maganhangzok = rovidMaganhangzok ++ hosszuMaganhangzok
 
+-- | Időmértékes szótagokra tagolja az adott szöveget.
+--
+-- Feltételezi, hagy a szöveg legfeljebb egy verssor (mivel sorok között nincs
+-- értelme átvinni ezt a fajta szótagot).
+--
+-- Wikipédiáról:
+--     "A szótag az időmértékes versritmusban a következő magánhangzóig terjed,
+--      függetlenül attól, hogy a következő magánhangzó ugyanabban a szóban vagy
+--      a rá következőben található.""
 idoSzotagol :: Text -> [Text]
-idoSzotagol szo =
-    let szokozNelkuli = T.filter nemSzokoz szo
+idoSzotagol szoveg =
+    let szokozNelkuli = T.filter nemSzokoz szoveg
     in tagol szokozNelkuli ""
   where
     nemSzokoz :: Char -> Bool
     nemSzokoz c = c /= ' '
-    --
-    tagol :: Text -> Text -> [Text]
-    tagol m aktualis = case T.uncons m of
-        Nothing -> [aktualis]
+    -- | Időalapú szótagokra bontja az adott szóköz nélküli szöveget.
+    -- Rekurzió során sorra veszi a szöveg karatereit, és vagy bővíti az
+    -- aktuális szótagot, vagy lezárja azt, ha új szótag magánhangzójával
+    -- találkozik.
+    tagol
+        :: Text  -- ^ A szöveg maradéka.
+        -> Text  -- ^ Az aktuális szótag gyűjtője.
+        -> [Text]
+    tagol s aktualis = case T.uncons s of
+        Nothing ->
+            -- Elfogyott a szöveg, szótag ami a gyűjtőben van.
+            [aktualis]
         Just (k, tobbi) ->
+            -- Ha mhzóval találkozunk, és már van mhzó a szótagunkban, lezárjuk
+            -- a szótagot és újat kezdünk. Különben bővítjük a gyűjtőt.
             if maganhangzo k && T.any maganhangzo aktualis
-                then aktualis : tagol m ""
+                then aktualis : tagol s ""
                 else tagol tobbi (T.snoc aktualis k)
-    --
+    -- | Igaz, ha az adott betű magánhangzó.
     maganhangzo :: Char -> Bool
     maganhangzo c = elem c maganhangzok
